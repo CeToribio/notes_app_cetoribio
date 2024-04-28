9ac4aa18bec96ae067a837db3b6c930f51506393
CeToribio <anctoribio@gmail.com>
2024-04-28 03:06:40 -0500

    feat: Implement note logic for the app

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,14 +1,119 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class HomeScreen extends StatelessWidget {
+import '../models/note.dart';
+import '../provider/note_provider.dart';
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  void _addNote(BuildContext context) async {
+    final TextEditingController controller = TextEditingController();
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext dialogContext) {
+        return AlertDialog(
+          title: const Text('Añadir Nota'),
+          content: TextField(
+            controller: controller,
+            decoration:
+                const InputDecoration(hintText: "Introduce el contenido de la nota"),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Cancelar'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: const Text('Añadir'),
+              onPressed: () {
+                // Añadir la nota a la base de datos
+                Provider.of<NoteProvider>(context, listen: false)
+                    .insert(controller.text);
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _editNote(BuildContext context, Note note) async {
+    final TextEditingController controller =
+        TextEditingController(text: note.content);
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext dialogContext) {
+        return AlertDialog(
+          title: const Text('Editar Nota'),
+          content: TextField(
+            controller: controller,
+            decoration:
+                const InputDecoration(hintText: "Edita el contenido de la nota"),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Cancelar'),
+              onPressed: () {
+                Navigator.of(dialogContext).pop();
+              },
+            ),
+            TextButton(
+              child: const Text('Guardar'),
+              onPressed: () {
+                Provider.of<NoteProvider>(context, listen: false)
+                    .update(note.id, controller.text);
+                Navigator.of(dialogContext).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _deleteNote(BuildContext context, Note note) async {
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext dialogContext) {
+        return AlertDialog(
+          title: const Text('Eliminar Nota'),
+          content: const Text("¿Estás seguro de que quieres eliminar esta nota?"),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Cancelar'),
+              onPressed: () {
+                Navigator.of(dialogContext).pop();
+              },
+            ),
+            TextButton(
+              child: const Text('Eliminar'),
+              onPressed: () {
+                Provider.of<NoteProvider>(context, listen: false)
+                    .delete(note.id);
+                Navigator.of(dialogContext).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text(
-          'Notes',
+          'Notas',
           style: TextStyle(
               color: Colors.white,
               fontSize: 24.0,
@@ -17,9 +122,50 @@ class HomeScreen extends StatelessWidget {
         elevation: 2,
         backgroundColor: Theme.of(context).colorScheme.primary,
       ),
-      body: const Center(
-        child: Text('Crea tu primera nota'),
+      body: Consumer<NoteProvider>(
+        builder: (context, noteProvider, child) {
+          return ListView.builder(
+            itemCount: noteProvider.notes.length,
+            itemBuilder: (context, index) {
+              final note = noteProvider.notes[index];
+              return ListTile(
+                title: Text(note.content),
+                trailing: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    IconButton(
+                      icon: const Icon(Icons.edit),
+                      onPressed: () {
+                        _editNote(context, note);
+                      },
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.delete),
+                      onPressed: () {
+                        _deleteNote(context, note);
+                      },
+                    ),
+                  ],
+                ),
+              );
+            },
+          );
+        },
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _addNote(context),
+        child: const Icon(Icons.add),
+      ),
+      // body: const Center(
+      //   child: Column(
+      //     mainAxisAlignment: MainAxisAlignment.center,
+      //     children: [
+      //       Text('Crea tu primera nota'),
+      //       SizedBox(height: 20.0),
+      //       Icon(Icons.note_add, size: 50.0, color: Colors.grey),
+      //     ],
+      //   ),
+      // ),
     );
   }
 }
